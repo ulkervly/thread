66a220fea0baad92c8057e72972465c5ee567b8a
ulkervly <bd7ms92nx@code.edu.az>
2023-10-27 12:07:42 +0400

    first commit

---
+++ Models/Shop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread.Models
+{
+    public class Shop
+    {
+        private List<Order> Orders = new List<Order>();
+
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+        public double GetOrdersAvg()
+        {
+            double sumPrice = 0;
+         
+            foreach (Order order in Orders)
+            {
+                sumPrice+= order.TotalAmount;
+                
+            }
+            return sumPrice/Orders.Count;
+        }
+        //public void GetOrdersAvg(DateTime dateTime)
+        //{
+        //    foreach (Order order in Orders)
+        //    {
+
+        //    }
+
+        //}
+        public void RemoveOrderByNo(int no)
+        {
+            Orders.RemoveAll(order=>order.No == no);   
+               
+
+        }
+        public List<Order> FilterOrderByPrice(int minPrice,int maxprice)
+        {
+            List<Order> filteredOrders = new List<Order>();
+            foreach (Order order in Orders)
+            {
+                if (order.TotalAmount>minPrice && order.TotalAmount<maxprice)
+                {
+                    filteredOrders.Add(order);
+                }
+            }
+            return filteredOrders;
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Thread.Models;
+
+namespace Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+
+          
+            shop.AddOrder(new Order { No = 1, ProductCount = 2, TotalAmount = 100 });
+            shop.AddOrder(new Order { No = 2, ProductCount = 3, TotalAmount = 200 });
+           
+
+            
+            double avgOrderValue = shop.GetOrdersAvg();
+
+            
+            
+
+            
+            int minPrice = 100;
+            int maxPrice = 200;
+            List<Order> filteredOrders = shop.FilterOrderByPrice(minPrice, maxPrice);
+
+        }
+    }
+}
